a3d6e059d0e70e17da55cbce843794892df07c47
Alpha Olomi <alphaolomi@gmail.com>
2024-08-21 16:07:44 +0300

    un-comment tests and fixtures

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -23,47 +23,47 @@ it('can instantiate Api class', function () {
     expect($this->api)->toBeInstanceOf(Api::class);
 });
 
-// it('can list users', function () {
-//     $this->mockClient->addResponse(MockResponse::fixture('users.index'));
-//     $response = $this->api->users()->list();
-//     expect($response)->toBeArray();
-// });
+it('can list users', function () {
+    $this->mockClient->addResponse(MockResponse::fixture('users.index'));
+    $response = $this->api->users()->list();
+    expect($response)->toBeArray();
+});
 
-// it('can create a user', function () {
-//     $this->mockClient->addResponse(MockResponse::fixture('users.create'));
-//     $userStr = <<<'JSON'
-// {
-//     "name": "Leanne Graham",
-//     "username": "Bret",
-//     "email": "Sincere@april.biz",
-//     "address": {
-//         "street": "Kulas Light",
-//         "suite": "Apt. 556",
-//         "city": "Gwenborough",
-//         "zipcode": "92998-3874",
-//         "geo": { "lat": "-37.3159", "lng": "81.1496" }
-//     },
-//     "phone": "1-770-736-8031 x56442",
-//     "website": "hildegard.org",
-//     "company": {
-//         "name": "Romaguera-Crona",
-//         "catchPhrase": "Multi-layered client-server neural-net",
-//         "bs": "harness real-time e-markets"
-//     }
-// }
-// JSON;
-//     $response = $this->api->users()->create(json_decode($userStr, true));
-//     expect($response)->toBeArray();
-// });
+it('can create a user', function () {
+    $this->mockClient->addResponse(MockResponse::fixture('users.create'));
+    $userStr = <<<'JSON'
+{
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": { "lat": "-37.3159", "lng": "81.1496" }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+    }
+}
+JSON;
+    $response = $this->api->users()->create(json_decode($userStr, true));
+    expect($response)->toBeArray();
+});
 
-// it('can get a user', function () {
-//     $this->mockClient->addResponse(MockResponse::fixture('users.show'));
-//     $response = $this->api->users()->get((string) 1);
-//     expect($response)->toBeArray();
-// });
+it('can get a user', function () {
+    $this->mockClient->addResponse(MockResponse::fixture('users.show'));
+    $response = $this->api->users()->get((string) 1);
+    expect($response)->toBeArray();
+});
 
-// it('can delete a user', function () {
-//     $this->mockClient->addResponse(MockResponse::fixture('users.delete'));
-//     $response = $this->api->users()->delete((string) 2);
-//     expect($response)->toBeArray();
-// });
+it('can delete a user', function () {
+    $this->mockClient->addResponse(MockResponse::fixture('users.delete'));
+    $response = $this->api->users()->delete((string) 2);
+    expect($response)->toBeArray();
+});
